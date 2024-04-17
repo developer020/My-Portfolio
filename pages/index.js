789dfb10c2f304e6093fcce9a2b6bc6e57312f7a
Zamzam Ali <110178425+developer020@users.noreply.github.com>
2024-04-17 16:44:37 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -133,8 +133,8 @@ export default function Home() {
                         alt="image"
                         src={web1} 
                         className="rounded-lg object-cover" 
-                        width={"50%"} 
-                        height={"50%"} 
+                        width={"100%"} 
+                        height={"100%"} 
                         layout="responsive"
                       />
                         <div className="rounded-lg absolute inset-0 flex items-center justify-center bg-black bg-opacity-75 opacity-0 hover:opacity-100 transition duration-300">
